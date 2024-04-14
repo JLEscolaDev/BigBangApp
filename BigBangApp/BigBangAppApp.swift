e3db909b108942e4f777e6e287f3d09c8db04b04
Jose Luis Escolá García <jletrabajoapple@gmail.com>
2024-04-14 10:24:59 +0200

    Initial Commit

---
+++ BigBangApp/BigBangAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangAppApp.swift
+//  BigBangApp
+//
+//  Created by Jose Luis Escolá García on 14/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBangAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
